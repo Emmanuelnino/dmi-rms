4d5a668d74fa5f3e2c9600649a08cc4e6379102e
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-23 07:24:20 -0700

    change

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,46 +1,46 @@
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 
-const prisma =new PrismaClient()
+const prisma = new PrismaClient()
 
 
-async function Student (req,res){
-    try{
-        const{regno,name,password}=req.body
+async function Student(req, res) {
+    try {
+        const { regno, name, password } = req.body
         const newStudent = await prisma.student.create({
-            data:{
+            data: {
                 regno,
                 name,
                 password
             },
-                
+
         });
         res.json(newStudent);
-    }catch(error){
-        res.status(500).json({error:'internal server error'})
+    } catch (error) {
+        res.status(500).json({ error: 'internal server error' })
     }
-} 
+}
 
-async function Lecturer (req,res){
-    try{
-        const{regno,name,password}=req.body
+async function Lecturer(req, res) {
+    try {
+        const { regno, name, password } = req.body
         const newLecturer = await prisma.lecturer.create({
-            data:{
+            data: {
                 regno,
                 name,
                 password
             },
         });
         res.json(newLecturer);
-    }catch(error){
-        res.status(500).json({error:'internal server error'})
+    } catch (error) {
+        res.status(500).json({ error: 'internal server error' })
     }
 }
 
-async function Results (req,res){
-    try{
-        const{regno,name,semester,code,mark,gpa}=req.body
+async function Results(req, res) {
+    try {
+        const { regno, name, semester, code, mark, gpa } = req.body
         const newResults = await prisma.result.create({
-            data:{
+            data: {
                 regno,
                 name,
                 semester,
@@ -50,10 +50,68 @@ async function Results (req,res){
             },
         });
         res.json(newResults);
-    }catch(error){
-        res.status(500).json({error:'internal server error'})
+    } catch (error) {
+        res.status(500).json({ error: 'internal server error' })
+    }
+}
+async function updateUser(req, res) {
+
+    const { id } = req.params;
+    const { regno, name, password } = req.body
+
+
+    try {
+        const updateUser = await prisma.student.update({
+            where: { id: parseInt(id) },
+            data: {
+                name: name,
+                regno: regno,
+                password: password
+            },
+        });
+        res.status(200).json(updateUser);
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ error: 'An error occurred while updating user.' });
+    }
+
+}
+async function updateUser(req, res) {
+
+    const { id } = req.params;
+    const { regno, name, password } = req.body
+
+
+    try {
+        const updateUser = await prisma.student.update({
+            where: { id: parseInt(id) },
+            data: {
+                name: name,
+                regno: regno,
+                password: password
+            },
+        });
+        res.status(200).json(updateUser);
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ error: 'An error occurred while updating user.' });
+    }
+
+}
+async function deleteUser(req, res) {
+
+    const { id } = req.params;
+    try {
+        await prisma.lecturer.delete({
+            where: { id: parseInt(id) },
+        });
+        res.status(200).json("User deleted successfully");
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        res.status(500).json({ error: 'An error occurred while deleting user.' });
+
     }
 }
 
 
-module.exports={Student,Lecturer,Results};
+module.exports = { Student, Lecturer, Results, updateUser, deleteUser };
--- Routes/route.js
+++ Routes/route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {Student,Lecturer, Results}= require('../Controller/controller');
-const userController = require('./userController');
+const {Student,Lecturer, Results, updateUser, deleteUser}= require('../Controller/controller');
+
 
 router.post('/student', Student);
 router.post('/lecturer', Lecturer);
 router.post('/result',Results);
-router.put('/users/:id',userController.updateUser);
+router.put('/student/:id',updateUser);
+router.delete('/student/:id',deleteUser);
+router.delete('/lecturer/:id',deleteUser);
 
 module.exports = router; 
