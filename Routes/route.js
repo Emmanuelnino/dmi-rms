a5f18c552a493f985d21bd4f24d863b560a1e413
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-22 23:50:47 -0700

    changes

--- Routes/route.js
+++ Routes/route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {Student,Lecturer, Results}= require('../Controller/controller');
-
+const userController = require('./userController');
 
 router.post('/student', Student);
 router.post('/lecturer', Lecturer);
 router.post('/result',Results);
+router.put('/users/:id',userController.updateUser);
 
 module.exports = router; 
